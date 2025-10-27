82e938d315a96559793d2f2b03ea3df9a6c54399
Shivansh Kushwah <s.kushwah9898@gmail.com>
2025-10-27 14:08:21 +0530

    Commit

--- app/src/main/java/com/example/institutional_app_schools_colleges/MainActivity.java
+++ app/src/main/java/com/example/institutional_app_schools_colleges/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.institutional_app_schools_colleges;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.text);
 
     }
 }
